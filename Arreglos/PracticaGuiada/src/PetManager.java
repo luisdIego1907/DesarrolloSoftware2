acb6592fcb79a35c2c5b5049ed51759b889cbc36
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-01-14 09:06:51 -0600

    Implementacion del metodo removePet

--- Arreglos/PracticaGuiada/src/PetManager.java
+++ Arreglos/PracticaGuiada/src/PetManager.java
@@ -26,6 +26,31 @@ public class PetManager {
         return false;
     }
 
+    public boolean removePet(Pet pet){
+
+        for(int i = 0; i < pets.length; i++){
+    
+            if(pets[i] != null && pets[i].equals(pet)){
+
+                //Corre a la izquierda los elementos posteriores al eliminado.
+                //De esta forma, el arreglo no queda con "brincos" o espacios
+                // en null
+                for(int j = i; j < pets.length -1 ; j++){
+
+                    pets[j] = pets[j+1];
+                }
+
+                pets[pets.length - 1] = null;
+                
+                petCounter--;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public Pet[] getPets(){
         return this.pets;
     }
