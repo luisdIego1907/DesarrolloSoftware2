0ea97d444fcec4d7d997edd1307bd617ce2425f5
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-01-14 11:10:07 -0600

    Se creo el metodo updatePets, isEmpty, addPet y agregarMoviendo

--- Arreglos/PracticaGuiada/src/PetManager.java
+++ Arreglos/PracticaGuiada/src/PetManager.java
@@ -24,22 +24,22 @@ public class PetManager {
         return false;
     }
 
-    public boolean removePet(Pet pet){
+    public boolean removePet(Pet pet) {
 
-        for(int i = 0; i < pets.length; i++){
-    
-            if(pets[i] != null && pets[i].equals(pet)){
+        for (int i = 0; i < pets.length; i++) {
 
-                //Corre a la izquierda los elementos posteriores al eliminado.
-                //De esta forma, el arreglo no queda con "brincos" o espacios
+            if (pets[i] != null && pets[i].equals(pet)) {
+
+                // Corre a la izquierda los elementos posteriores al eliminado.
+                // De esta forma, el arreglo no queda con "brincos" o espacios
                 // en null
-                for(int j = i; j < pets.length -1 ; j++){
+                for (int j = i; j < pets.length - 1; j++) {
 
-                    pets[j] = pets[j+1];
+                    pets[j] = pets[j + 1];
                 }
 
                 pets[pets.length - 1] = null;
-                
+
                 petCounter--;
 
                 return true;
@@ -49,14 +49,76 @@ public class PetManager {
         return false;
     }
 
-    public Pet[] getPets(){
-        
+    public Pet[] getPets() {
+
         Pet[] pets = new Pet[this.petCounter];
 
-        for(int i = 0; i < this.petCounter; i++){
+        for (int i = 0; i < this.petCounter; i++) {
             pets[i] = this.pets[i];
         }
 
         return pets;
+    }
+
+    public boolean updatePets(Pet pet, int index) {
+
+        if ((index < 0) || (index > petCounter) || pet == null) {
+
+            return false;
+
+        } else if (isEmpty(pet, index)) {
+
+            return addPet(pet, index);
+
+        } else {
+
+            return agregarMoviendo(pet, index);
+
+        }
+
+    }
+
+    public boolean isEmpty(Pet pet, int index) {
+
+        for (int i = 0; i < petCounter; i++) {
+
+            if (this.pets[i] == null) {
+
+                return true;
+            }
+        }
+        IOManager.print("False en isEmpty");
+        return false;
+    }
+
+    public boolean addPet(Pet pet, int index) {
+
+        this.pets[index] = pet;
+
+        petCounter++;
+
+        return true;
+    }
+
+    public boolean agregarMoviendo(Pet pet, int index) {
+
+        for (int i = 0; i < pets.length; i++) {
+
+            if (i == index) {
+
+                Pet provicional = pets[i];
+
+                pets[i] = pet;
+
+                pets[i + 1] = provicional;
+
+                petCounter++;
+
+                return true;
+            }
+        }
+
+        IOManager.print("false en agregarMoviendo");
+        return false;
     }
 }
