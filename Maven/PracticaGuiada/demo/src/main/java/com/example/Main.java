a0dc0eacfc7b5a91288dd32e0db72fe38829c353
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-01-21 11:14:32 -0600

    Fin clase 21 / 01

--- Maven/PracticaGuiada/demo/src/main/java/com/example/Main.java
+++ Maven/PracticaGuiada/demo/src/main/java/com/example/Main.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import com.google.gson.Gson;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Gson gson = new Gson();
+
+        Person person = new Person("Luis" , 20 , true);
+
+        String json = gson.toJson(person);
+
+        System.out.print(json);
+        //Continuar diapo 32
     }
 }
--- Maven/PracticaGuiada/demo/src/main/java/com/example/Person.java
+++ Maven/PracticaGuiada/demo/src/main/java/com/example/Person.java
@@ -16,5 +16,29 @@ public class Person {
         this.isStudent = isStudent;
     }
 
-    
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean getIsStudent(){
+        return isStudent;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setIsStudent(boolean isStudent){
+        this.isStudent = isStudent;
+    }
+
+    @Override
+    public String toString(){
+
+        return " Name: " + name + " Edad: " + age + " isStudent: " + isStudent;
+    }
 }
