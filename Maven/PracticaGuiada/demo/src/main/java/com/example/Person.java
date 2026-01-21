04ca33f7796d9a857ad34d74eb1a1410d4c2a9b2
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-01-21 10:58:52 -0600

    Clase y practica guiada

---
+++ Maven/PracticaGuiada/demo/src/main/java/com/example/Person.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private boolean isStudent;
+
+    public Person(){
+
+    }
+
+    public Person(String name , int age , boolean isStudent){
+        this.name = name;
+        this.age = age;
+        this.isStudent = isStudent;
+    }
+
+    
+}
